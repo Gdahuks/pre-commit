d2c419ddd49766c8ed2bbf1309cabd4c6c31d1c4
Hubert Książek <mail.osobisty@tutanota.com>
2023-10-07 14:08:46 +0200

    Test

---
+++ first_package/__init__.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+def insecure_function(user_input):
+    os.system(f"rm -rf {user_input}")
+
+user_input  = input( "Podaj ścieżkę do pliku: ") # bardzo dluuguuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuugi komentarz
+insecure_function(user_input)
